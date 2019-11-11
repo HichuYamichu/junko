0a6559f1ffba5c13f8a623cddce52f57bb619a09
HichuYamichu <hichuyamichu@gmail.com>
2019-11-11 18:12:16 +0100

    config changes

--- src/dashboard/nuxt.config.js
+++ src/dashboard/nuxt.config.js
@@ -18,12 +18,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: 'Discord bot',
+        name: 'Junko dashboard',
+        content: 'Touhou themed discord bot'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/avatar.webp' }]
   },
 
   loading: { color: '#fff' },
