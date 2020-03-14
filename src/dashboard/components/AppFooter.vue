ff5a8873934cf2151ca69204bfde456b6799f06b
HichuYamichu <hichuyamichu@gmail.com>
2020-03-14 16:35:43 +0000

    copyright update

--- src/dashboard/components/AppFooter.vue
+++ src/dashboard/components/AppFooter.vue
@@ -1,5 +1,13 @@
 <template>
   <v-footer app absolute color="black">
-    <span class="white--text">&copy; 2019 Hichu-Yamichu</span>
+    <span class="white--text">&copy; 2019-{{ date }} Hichu-Yamichu</span>
   </v-footer>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    date: new Date().getFullYear()
+  })
+};
+</script>
